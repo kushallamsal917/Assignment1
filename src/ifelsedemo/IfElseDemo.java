8314d680cccacfc5c4cbbc299d0ae7ab0bbbb1b4
mustafasahak <msahak@icloud.com>
2021-07-16 14:01:33 -0400

    added IF ELSE Demo

---
+++ src/ifelsedemo/IfElseDemo.java
@@ -0,0 +1,29 @@
+package ifelsedemo;
+
+import java.util.Scanner;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		// Im trying to do the if else demo
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Please enter your number");
+		
+		
+		int i= sc.nextInt();
+	
+		
+			if (i> 20)
+			{
+				System.out.println("this is worong");
+			}
+			else 
+			{
+				System.out.println(" this is right");
+		
+	}
+
+	}
+
+}
